d4550a213b5927d1378b2dcc77081a8d47a5f5dc
hp9104 <160164785+hp9104@users.noreply.github.com>
2024-02-15 23:21:09 +0530

    Create  1a.java

---
+++ README.java
@@ -0,0 +1,10 @@
+public class Prac_1a{
+	public static void main(String[] s){
+		System.out.println("NAME : HARSHIT PANCHOLI");
+		System.out.println("ENROLLMENT NO. : 220410107026\n");
+		
+		System.out.println("WELCOME TO JAVA");
+		System.out.println("LEARNING JAVA NOW");
+		System.out.println("PROGRAMMING IS FUN");
+	}
+}
