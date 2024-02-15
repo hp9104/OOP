192cf0e41d5228e6b6569914d11246ccd3a59095
hp9104 <160164785+hp9104@users.noreply.github.com>
2024-02-15 23:22:45 +0530

    Create 1b.java

---
+++ 1b.java
@@ -0,0 +1,14 @@
+public class Prac_1b{
+	public static void main(String[] s){
+		System.out.println("NAME : HARSHIT PANCHOLI");
+		System.out.println("ENROLLMENT NO. : 220410107026\n");
+		
+		double a = 3.4 , b = 50.2 , e = 44.5 ;
+		double c = 2.1 , d = 0.55 , f = 5.9 ;
+		
+		double x = ((e * d) - (b * f)) / ((a * d) - (b * c));
+		double y = ((a * f) - (e * c)) / ((a * d) - (b * c));
+		
+		System.out.println("The value of X is " + x + " & Y is " + y );
+	}
+}
